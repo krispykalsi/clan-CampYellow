6992988d9fddbfc7a606c15ff296092fff9105de
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2022-07-12 04:46:21 +0530

    modify nav bar to match given ui, add profile icon

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,10 @@ class ClanApp extends StatelessWidget {
       home: const Scaffold(
         body: SafeArea(child: HomePage()),
         backgroundColor: Colors.black,
-        bottomNavigationBar: CustomNavigationBar(),
+        bottomNavigationBar: CustomNavigationBar(
+          profileImage: NetworkImage(
+              "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"),
+        ),
       ),
     );
   }
--- lib/ui/custom_navigation_bar.dart
+++ lib/ui/custom_navigation_bar.dart
@@ -1,35 +1,45 @@
 import 'package:flutter/material.dart';
 
 class CustomNavigationBar extends StatelessWidget {
-  const CustomNavigationBar({Key? key}) : super(key: key);
+  final ImageProvider? profileImage;
+
+  const CustomNavigationBar({Key? key, this.profileImage}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      height: 60,
-      labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-      destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.home),
-          label: "Home",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.star),
-          label: "Achievements",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.leaderboard),
-          label: "Leaderboard",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.group),
-          label: "Discussions",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.person_rounded),
-          label: "Profile",
-        ),
-      ],
+    return NavigationBarTheme(
+      data: NavigationBarThemeData(
+        indicatorColor: Colors.transparent,
+        iconTheme: MaterialStateProperty.all(const IconThemeData(size: 40)),
+      ),
+      child: NavigationBar(
+        height: 60,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        destinations: [
+          const NavigationDestination(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          const NavigationDestination(
+            icon: Icon(Icons.star),
+            label: "Achievements",
+          ),
+          const NavigationDestination(
+            icon: Icon(Icons.leaderboard),
+            label: "Leaderboard",
+          ),
+          const NavigationDestination(
+            icon: Icon(Icons.group),
+            label: "Discussions",
+          ),
+          NavigationDestination(
+            icon: profileImage == null
+                ? const Icon(Icons.person)
+                : CircleAvatar(backgroundImage: profileImage),
+            label: "Profile",
+          ),
+        ],
+      ),
     );
   }
 }
